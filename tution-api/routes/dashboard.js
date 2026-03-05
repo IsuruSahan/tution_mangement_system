81c9dd8b59af6b6d4a64a4c60d4b13b7c1ffaabf
IsuruSahan <12isurukumarasiri@gmail.com>
2026-03-05 10:36:38 +0530

    Revert "fix(dashboard): correct typo in comment for chart data"
    This reverts commit 49f411e7af8b1d58645c229deb08674aa7b722e5.

--- tution-api/routes/dashboard.js
+++ tution-api/routes/dashboard.js
@@ -164,7 +164,7 @@ router.get('/', async (req, res) => {
             presentTodayByGrade,
             pendingPaymentsByGrade,
             
-            // Chart dataa
+            // Chart data
             paymentStatusThisMonth
         });
 
